6bcbc1fbf0c0f3bd6d3e0f0894546e0765eab5fb
Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>
2024-02-03 18:27:56 -0500

    normal faces

--- src/transforms/make_normal_face_dataset.py
+++ src/transforms/make_normal_face_dataset.py
@@ -1,25 +1,61 @@
-from pyspark.sql import SparkSession, DataFrame
-from src.constants import NORMAL_FACES_IMAGES_PATH
+from pyspark.sql import SparkSession, DataFrame, functions as F
+from enum import Enum
+
+# Run from the src/ folder
+NORMAL_FACES_IMAGES_PATH = "../data/normal_faces/lfw-deepfunneled/lfw-deepfunneled/"
+
+NAMES_PATH = "../data/normal_faces/lfw_allnames.csv"
+
+OUTPUT_DATASET_PATH = "../data/normal_faces/datasets/normal_faces"
+
+DF_SIZE = 100
 
 spark = SparkSession.builder.getOrCreate()
 
+
 def main():
-    df: DataFrame = spark.read.format("image").option("dropInvalid", True).option("recursiveFileLookup", "true").load(NORMAL_FACES_IMAGES_PATH)
+    image_df: DataFrame = spark.read.format("image").option("dropInvalid", True).option("recursiveFileLookup", "true").load(NORMAL_FACES_IMAGES_PATH)
+    names_df: DataFrame = spark.read.format("csv").option("header", True).load(NAMES_PATH)
     df = (
-        df
+        image_df
         .select(
             "image.origin",
             "image.height",
             "image.width",
             "image.nChannels",
-            "image.data",
+            F.col("image.data").alias("image_data"),
+            F.lit(False).alias("is_deepfake"),
+        )
+        .withColumn(
+            "person_name",
+            F.element_at(F.split("origin", "/"), -2)
+        )
+        .join(
+            names_df.select(F.col("name").alias("person_name"), F.col("images").alias("image_count_for_person")),
+            on="person_name",
+            how="left"
+        )
+        .withColumn(
+            "image_id",
+            F.monotonically_increasing_id()
         )
     )
-    print(df.select("image.origin",
-            "image.height",
-            "image.width",
-            "image.nChannels").head())
-    print(df.count())
+
+    # For our current purposes, limit to 100 rows
+    df = (
+        df
+        .withColumn(
+            "__random",
+            F.rand(seed=43)
+        )
+        .orderBy("__random")
+        .limit(DF_SIZE)
+    )
+
+    print("Writing dataframe...")
+    df.write.parquet(OUTPUT_DATASET_PATH, mode="overwrite")
+    print("Done!")
+    
 
 if __name__ == "__main__":
     main()
